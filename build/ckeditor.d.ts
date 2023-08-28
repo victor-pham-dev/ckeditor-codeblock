3c10120e69d9c98273591d261a42f5f60006b354
victor <truongpham2412.nd@gmail.com>
2023-08-28 13:58:49 +0700

    update builder

--- build/ckeditor.d.ts
+++ build/ckeditor.d.ts
@@ -2,20 +2,20 @@
  * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
-import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
-import { Alignment } from '@ckeditor/ckeditor5-alignment';
-import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
-import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
-import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
-import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { Heading } from '@ckeditor/ckeditor5-heading';
-import { Indent } from '@ckeditor/ckeditor5-indent';
-import { Link } from '@ckeditor/ckeditor5-link';
-import { List } from '@ckeditor/ckeditor5-list';
-import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
-import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+import { ClassicEditor } from "@ckeditor/ckeditor5-editor-classic";
+import { Alignment } from "@ckeditor/ckeditor5-alignment";
+import { Bold, Italic } from "@ckeditor/ckeditor5-basic-styles";
+import { BlockQuote } from "@ckeditor/ckeditor5-block-quote";
+import { CodeBlock } from "@ckeditor/ckeditor5-code-block";
+import { Essentials } from "@ckeditor/ckeditor5-essentials";
+import { Heading } from "@ckeditor/ckeditor5-heading";
+import { Indent } from "@ckeditor/ckeditor5-indent";
+import { Link } from "@ckeditor/ckeditor5-link";
+import { List } from "@ckeditor/ckeditor5-list";
+import { Paragraph } from "@ckeditor/ckeditor5-paragraph";
+import { Table, TableToolbar } from "@ckeditor/ckeditor5-table";
 declare class Editor extends ClassicEditor {
-    static builtinPlugins: (typeof Alignment | typeof Bold | typeof Italic | typeof BlockQuote | typeof CodeBlock | typeof Essentials | typeof Paragraph | typeof Heading | typeof Indent | typeof Link | typeof List | typeof Table | typeof TableToolbar)[];
+    static builtinPlugins: (typeof Alignment | typeof BlockQuote | typeof Bold | typeof CodeBlock | typeof Essentials | typeof Heading | typeof Indent | typeof Italic | typeof Link | typeof List | typeof Paragraph | typeof Table | typeof TableToolbar)[];
     static defaultConfig: {
         toolbar: {
             items: string[];
--- src/ckeditor.ts
+++ src/ckeditor.ts
@@ -3,70 +3,66 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+import { ClassicEditor } from "@ckeditor/ckeditor5-editor-classic";
 
-import { Alignment } from '@ckeditor/ckeditor5-alignment';
-import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
-import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
-import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
-import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { Heading } from '@ckeditor/ckeditor5-heading';
-import { Indent } from '@ckeditor/ckeditor5-indent';
-import { Link } from '@ckeditor/ckeditor5-link';
-import { List } from '@ckeditor/ckeditor5-list';
-import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
-import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+import { Alignment } from "@ckeditor/ckeditor5-alignment";
+import { Bold, Italic } from "@ckeditor/ckeditor5-basic-styles";
+import { BlockQuote } from "@ckeditor/ckeditor5-block-quote";
+import { CodeBlock } from "@ckeditor/ckeditor5-code-block";
+import { Essentials } from "@ckeditor/ckeditor5-essentials";
+import { Heading } from "@ckeditor/ckeditor5-heading";
+import { Indent } from "@ckeditor/ckeditor5-indent";
+import { Link } from "@ckeditor/ckeditor5-link";
+import { List } from "@ckeditor/ckeditor5-list";
+import { Paragraph } from "@ckeditor/ckeditor5-paragraph";
+import { Table, TableToolbar } from "@ckeditor/ckeditor5-table";
 
 // You can read more about extending the build with additional plugins in the "Installing plugins" guide.
 // See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
 
 class Editor extends ClassicEditor {
-	public static override builtinPlugins = [
-		Alignment,
-		BlockQuote,
-		Bold,
-		CodeBlock,
-		Essentials,
-		Heading,
-		Indent,
-		Italic,
-		Link,
-		List,
-		Paragraph,
-		Table,
-		TableToolbar
-	];
+  public static override builtinPlugins = [
+    Alignment,
+    BlockQuote,
+    Bold,
+    CodeBlock,
+    Essentials,
+    Heading,
+    Indent,
+    Italic,
+    Link,
+    List,
+    Paragraph,
+    Table,
+    TableToolbar,
+  ];
 
-	public static override defaultConfig = {
-		toolbar: {
-			items: [
-				'heading',
-				'|',
-				'bold',
-				'italic',
-				'link',
-				'bulletedList',
-				'numberedList',
-				'|',
-				'outdent',
-				'indent',
-				'|',
-				'codeBlock',
-				'insertTable',
-				'blockQuote',
-				'undo',
-				'redo'
-			]
-		},
-		language: 'en',
-		table: {
-			contentToolbar: [
-				'tableColumn',
-				'tableRow',
-				'mergeTableCells'
-			]
-		}
-	};
+  public static override defaultConfig = {
+    toolbar: {
+      items: [
+        "heading",
+        "|",
+        "bold",
+        "italic",
+        "link",
+        "bulletedList",
+        "numberedList",
+        "|",
+        // 'outdent',
+        // 'indent',
+        "|",
+        "codeBlock",
+        "insertTable",
+        "blockQuote",
+        "undo",
+        "redo",
+      ],
+    },
+    language: "en",
+    table: {
+      contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+    },
+  };
 }
 
 export default Editor;
